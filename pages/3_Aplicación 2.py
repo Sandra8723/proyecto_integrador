f90a831535eeed36ab4222b7ed0f8c38869737f7
sandra <sandrahiguitatoro@gmail.com>
2023-10-06 09:41:01 -0500

    c2

--- pages/3_Aplicación 2.py
+++ pages/3_Aplicación 2.py
@@ -4,16 +4,22 @@ import platform
 # Diseño personalizado
 st.header("Aplicación 2")
 
-st.number_input 
 def main():
-    system_info = platform.uname()
+    st.title ("Información del sistema")
     
-    print("Información del sistema:")
-    print(f"Sistema operativo: {system_info.system}")
-    print(f"Versión: {system_info.version}")
-    print(f"Nombre del nodo: {system_info.node}")
-    print(f"Arquitectura: {system_info.machine}")
-    print(f"Procesador: {system_info.processor}")
+    system_info = platform.uname()
+
+    st.write("Sistema operativo:", 
+    system_info.system), 
+    system_info = platform.uname(),
+    system_info = platform.un
+    system_info = platform
+    system_info = system_info
+   
+    st.write("Versión:", system_info.version)
+    st.write("Nombre del nodo:", system_info.node)
+    st.write("Arquitectura:", system_info.machine)
+    st.write("Procesador:", system_info.processor)
 
 if __name__ == "__main__":
     main()
